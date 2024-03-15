7b8c37c6e834abfb634392d965913377091f273a
dragmove <dragmove@gmail.com>
2024-03-16 01:46:11 +0900

    feat: add api fetching following tutorial post

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
+// @see https://www.digitalocean.com/community/tutorials/how-to-implement-caching-in-node-js-using-redis
+
 const express = require('express');
+const axios = require('axios');
+
+/*
 const redis = require('redis');
 
 const client = redis.createClient({
-    host:'redis-server',
-    port: 6379
+  host: 'redis-server',
+  port: 6379,
 });
 client.on('connect', () => {
-    console.log(`redis-server is connected. set 'foo' to 0.`);
-    client.set('foo', 0);
+  console.log(`redis-server is connected. set 'foo' to 0.`);
+  client.set('foo', 0);
 });
 client.on('error', (error) => {
-    console.error(error);
+  console.error(error);
 });
+*/
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
+  res.send('hello');
+  /*
     client.get('foo', (error, value) => {
         if(error) {
             console.error(error);
@@ -26,8 +36,37 @@ app.get('/', (req, res) => {
 
         client.set('foo', parseInt(value, 10) + 1);
     });
+    */
 });
 
-app.listen(8080, () => {
-    console.log('Listening on port 8080');
+app.get('/fish/:species', getSpeciesData);
+
+app.listen(port, () => {
+  console.log(`Express App Listening on port ${port}`);
 });
+
+async function getSpeciesData(req, res) {
+  const species = req.params.species;
+  let results;
+
+  try {
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw 'API returned an empty array';
+    }
+
+    res.send({
+      fromCache: false,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send('Data unavailable');
+  }
+}
+
+async function fetchApiData(species) {
+  const apiResponse = await axios.get(`https://www.fishwatch.gov/api/species/${species}`);
+  console.log('Request sent to the API');
+  return apiResponse.data;
+}
