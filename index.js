c6df14c5ddad67e1754a9fc4dcc6198289ee9c62
dragmove <dragmove@gmail.com>
2024-03-17 13:35:12 +0900

    feat: arrange middleware

--- index.js
+++ index.js
@@ -1,59 +1,73 @@
 // @see https://www.digitalocean.com/community/tutorials/how-to-implement-caching-in-node-js-using-redis
 
 const express = require('express');
+const { createClient } = require('redis');
 const axios = require('axios');
 
-/*
-const redis = require('redis');
+let redisClient;
 
-const client = redis.createClient({
-  host: 'redis-server',
-  port: 6379,
-});
-client.on('connect', () => {
-  console.log(`redis-server is connected. set 'foo' to 0.`);
-  client.set('foo', 0);
-});
-client.on('error', (error) => {
-  console.error(error);
-});
-*/
+(async () => {
+  redisClient = await createClient({ port: 6379 });
+  redisClient
+    .on('connect', () => {
+      console.log(`redis-server is connected`);
+    })
+    .on('error', (error) => {
+      console.error(error);
+    })
+    .connect();
+})();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('hello');
-  /*
-    client.get('foo', (error, value) => {
-        if(error) {
-            console.error(error);
-            res.send(`There is something wrong when redis client tried to get 'foo'`);
-            return;
-        }
-
-        res.send(`'foo': ${value} on redis-server will be increased each time you refresh this page.`); 
-
-        client.set('foo', parseInt(value, 10) + 1);
-    });
-    */
+  res.send('hello, redis practice');
 });
 
-app.get('/fish/:species', getSpeciesData);
+app.get('/fish/:species', cacheData, getSpeciesData);
 
 app.listen(port, () => {
   console.log(`Express App Listening on port ${port}`);
 });
 
-async function getSpeciesData(req, res) {
-  const species = req.params.species;
-  let results;
+async function cacheData(req, res, next) {
+  const { species } = req.params;
 
   try {
-    results = await fetchApiData(species);
-    if (results.length === 0) {
+    // return redis cache
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      res.send({
+        fromCache: true,
+        data: JSON.parse(cacheResults),
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(404);
+  }
+}
+
+async function getSpeciesData(req, res) {
+  const { species } = req.params;
+  console.log('species :', species);
+
+  try {
+    const results = await fetchApiData(species);
+    if (!results?.length) {
       throw 'API returned an empty array';
     }
+
+    // set redis cache
+    // @see https://www.npmjs.com/package/redis#redis-commands
+    // @see https://www.digitalocean.com/community/tutorials/how-to-implement-caching-in-node-js-using-redis#step-4-implementing-cache-validity
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 30, // accepts a value with the cache duration in seconds.
+      NX: true, // when set to true, it ensures that the set() method should only set a key that doesn’t already exist in Redis.
+    });
 
     res.send({
       fromCache: false,
@@ -70,3 +84,39 @@ async function fetchApiData(species) {
   console.log('Request sent to the API');
   return apiResponse.data;
 }
+
+/*
+async function getSpeciesData(req, res) {
+  const { species } = req.params;
+  let results;
+  let isCached = false;
+
+  try {
+    const cacheResults = await redisClient.get(species);
+    if (cacheResults) {
+      isCached = true;
+      results = JSON.parse(cacheResults);
+    } else {
+      results = await fetchApiData(species);
+      if (results.length === 0) {
+        throw 'API returned an empty array';
+      }
+
+      // @see https://www.npmjs.com/package/redis#redis-commands
+      // @see https://www.digitalocean.com/community/tutorials/how-to-implement-caching-in-node-js-using-redis#step-4-implementing-cache-validity
+      await redisClient.set(species, JSON.stringify(results), {
+        EX: 30, // accepts a value with the cache duration in seconds.
+        NX: true, // when set to true, it ensures that the set() method should only set a key that doesn’t already exist in Redis.
+      });
+    }
+
+    res.send({
+      fromCache: isCached,
+      data: results,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(404).send('Data unavailable');
+  }
+}
+*/
